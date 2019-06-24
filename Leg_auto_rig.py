755cbc90071cddd10f342b767f3a41b106d916fe
Pearse <pearsetoomey@gmail.com>
2019-06-24 08:02:06 +0100

    still working leg, just updating in case i need to work from my lap top

--- Leg_auto_rig.py
+++ Leg_auto_rig.py
@@ -27,17 +27,35 @@ anklePos = cmds.xform('ankle', query=True, worldSpace=True, translation=True)
 ballPos = cmds.xform('ball', query=True, worldSpace=True, translation=True)
 toePos = cmds.xform('toe', query=True, worldSpace=True, translation=True)
 
+# creating
+si = 'L_'
+un = ''
+
 
 pelvisJnt = cmds.joint(name='pelvis', absolute=True, radius=2, position=pelvisPos)
-hipJnt = cmds.joint(name='hip', absolute=True, radius=2, position=hipPos)
-kneeJnt = cmds.joint(name='knee', absolute=True, radius=2, position=kneePos)
-ankleJnt = cmds.joint(name='ankle', absolute=True, radius=2, position=anklePos)
-ballJnt = cmds.joint(name='ball', absolute=True, radius=2, position=ballPos)
-toeJnt = cmds.joint(name='toe', absolute=True, radius=2, position=toePos)
+
+hipJnt = cmds.joint(name= si + un + 'hip', absolute=True, radius=2, position=hipPos)
+kneeJnt = cmds.joint(name= si + un + 'knee', absolute=True, radius=2, position=kneePos)
+ankleJnt = cmds.joint(name= si + un + 'ankle', absolute=True, radius=2, position=anklePos)
+ballJnt = cmds.joint(name= si + un + 'ball', absolute=True, radius=2, position=ballPos)
+toeJnt = cmds.joint(name= si + un + 'toe', absolute=True, radius=2, position=toePos)
 
 cmds.select(pelvisJnt, hipJnt, kneeJnt, ankleJnt, ballJnt, toeJnt)
 cmds.joint(edit=True, orientJoint='xyz', secondaryAxisOrient='yup' )
 
+cmds.select(clear=True)
+pvHipJnt = cmds.joint(name= si + un + 'pvHip', absolute=True, radius=2, position=hipPos)
+pvKneeJnt = cmds.joint(name= si + un + 'pvKnee', absolute=True, radius=2, position=kneePos)
+pvAnkleJnt = cmds.joint(name= si + un + 'pvAnkle', absolute=True, radius=2, position=anklePos)
+pvBallJnt = cmds.joint(name= si + un + 'pvBall', absolute=True, radius=2, position=ballPos)
+pvToeJnt = cmds.joint(name= si + un + 'pvToe', absolute=True, radius=2, position=toePos)
+
+cmds.select(pvHipJnt, pvKneeJnt, pvAnkleJnt, pvBallJnt, pvToeJnt)
+cmds.joint(edit=True, orientJoint='xyz', secondaryAxisOrient='yup' )
+
+kneeLocTemp = cmds.spaceLocator(n='KneeLoc_TEMP')
+cmds.delete(cmds.pointConstraint(hipJnt, ankleJnt, kneeLocTemp))
+cmds.delete(cmds.aimConstraint(kneeJnt, kneeLocTemp, aimVector=(0,0,1)))
 
 # when createing the knee aim ctrl , constrain between hip and ankle , del, then aim constraint to the knee bone so i can be suer there won't be any popping
 # i can use this to set the offset on the polevectro leg as well
