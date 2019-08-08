c501f66b333a6064f234204e0b900a7b80db06c0
Pearse <pearsetoomey@gmail.com>
2019-08-08 08:02:13 +0100

    Stretch plane script almost done, update so I can polish it in work

--- Leg_auto_rig.py
+++ Leg_auto_rig.py
@@ -20,6 +20,14 @@ cmds.move(6.351,0.067,10.598)
 
 cmds.select(clear=True)
 
+# set side and unique name
+si = 'L_'
+un = ''
+
+
+''' building joints for leg '''
+
+# get position from locators
 pelvisPos = cmds.xform('COG', query=True, worldSpace=True, translation=True)
 hipPos = cmds.xform('hip', query=True, worldSpace=True, translation=True)
 kneePos = cmds.xform('knee', query=True, worldSpace=True, translation=True)
@@ -27,11 +35,18 @@ anklePos = cmds.xform('ankle', query=True, worldSpace=True, translation=True)
 ballPos = cmds.xform('ball', query=True, worldSpace=True, translation=True)
 toePos = cmds.xform('toe', query=True, worldSpace=True, translation=True)
 
-# creating
-si = 'L_'
-un = ''
+pvKneeLoc = cmds.spaceLocator(n='KneeLocTemp')
+pvCtrlLoc = cmds.spaceLocator(n='pvKneeCtrlLoc')
+cmds.parent(pvCtrlLoc,pvKneeLoc)
 
+cmds.delete(cmds.pointConstraint('hip', 'ankle', pvKneeLoc))
+cmds.select(pvKneeLoc, 'knee')
+cmds.align(x='mid' ,alignToLead=True)
+cmds.align(y='mid' ,alignToLead=True)
+cmds.move(0,0,20, pvCtrlLoc, localSpace=True)
+cmds.select(clear=True)
 
+# make bind joints
 pelvisJnt = cmds.joint(name='pelvis', absolute=True, radius=2, position=pelvisPos)
 
 hipJnt = cmds.joint(name= si + un + 'hip', absolute=True, radius=2, position=hipPos)
@@ -40,10 +55,12 @@ ankleJnt = cmds.joint(name= si + un + 'ankle', absolute=True, radius=2, position
 ballJnt = cmds.joint(name= si + un + 'ball', absolute=True, radius=2, position=ballPos)
 toeJnt = cmds.joint(name= si + un + 'toe', absolute=True, radius=2, position=toePos)
 
-cmds.select(pelvisJnt, hipJnt, kneeJnt, ankleJnt, ballJnt, toeJnt)
+cmds.select(hipJnt, kneeJnt, ankleJnt, ballJnt, toeJnt)
 cmds.joint(edit=True, orientJoint='xyz', secondaryAxisOrient='yup' )
 
 cmds.select(clear=True)
+
+#make IK joints
 pvHipJnt = cmds.joint(name= si + un + 'pvHip', absolute=True, radius=2, position=hipPos)
 pvKneeJnt = cmds.joint(name= si + un + 'pvKnee', absolute=True, radius=2, position=kneePos)
 pvAnkleJnt = cmds.joint(name= si + un + 'pvAnkle', absolute=True, radius=2, position=anklePos)
@@ -53,214 +70,16 @@ pvToeJnt = cmds.joint(name= si + un + 'pvToe', absolute=True, radius=2, position
 cmds.select(pvHipJnt, pvKneeJnt, pvAnkleJnt, pvBallJnt, pvToeJnt)
 cmds.joint(edit=True, orientJoint='xyz', secondaryAxisOrient='yup' )
 
-kneeLocTemp = cmds.spaceLocator(n='KneeLoc_TEMP')
-cmds.delete(cmds.pointConstraint(hipJnt, ankleJnt, kneeLocTemp))
-cmds.delete(cmds.aimConstraint(kneeJnt, kneeLocTemp, aimVector=(0,0,1)))
+ik_handle = cmds.ikHandle(startJoint=pvHipJnt, endEffector=pvAnkleJnt, name='LegIkRPsolver_ikh', sol = 'ikRPsolver')
+
+# cmds.delete(cmds.aimConstraint(kneeJnt, kneeLocTemp, aimVector=(0,0,1)))
 
 # when createing the knee aim ctrl , constrain between hip and ankle , del, then aim constraint to the knee bone so i can be suer there won't be any popping
 # i can use this to set the offset on the polevectro leg as well
 # just need to figure out how to code up the connections for a stretch leg
 
 
-cmds.select(kneeJnt, kneeLoc)
-cmds.align(x='mid' ,alignToLead=True)
-'''
-cmds.ikHandle(n="ikh_leg", sj="ikj_hip", ee="ikj_ankle", sol="ikRPsolver")
-cmds.ikHandle(n="ikh_ball", sj="ikj_ankle", ee="ikj_ball", sol="ikSCsolver")
-cmds.ikHandle(n="ikh_toe", sj="ikj_ball", ee="ikj_toe", sol="ikSCsolver")
-
-footGroups = ("grp_footPivot", "grp_heel", "grp_toe", "grp_ball", "grp_flap")
-
-for item in footGroups:
-    cmds.group(n=item, empty=1, world=1)
-
-hipPos = cmds.xform("ikj_hip", q=True, ws=True, t=True)
-anklePos = cmds.xform("ikj_ankle", q=True, ws=True, t=True)
-ballPos = cmds.xform("ikj_ball", q=True, ws=True, t=True)
-toePos = cmds.xform("ikj_toe", q=True, ws=True, t=True)
-
-cmds.xform("grp_toe", ws=True, t=toePos)
-cmds.xform("grp_ball", ws=True, t=ballPos)
-cmds.xform("grp_flap", ws=True, t=ballPos)
-
-cmds.parent('grp_heel', 'grp_footPivot')
-cmds.parent('grp_toe', 'grp_heel')
-cmds.parent('grp_ball', 'grp_toe')
-cmds.parent('grp_flap', 'grp_toe')
-cmds.parent('ikh_leg', 'grp_ball')
-cmds.parent('ikh_ball', 'grp_ball')
-cmds.parent('ikh_toe', 'grp_flap')
-
-# create control for the foot
-cmds.circle(nr=(0, 1, 0), r=3, n="ctrl_leg")
-cmds.xform("ctrl_leg", t=ballPos)
-cmds.parent('grp_footPivot', 'ctrl_leg')
-cmds.makeIdentity('ctrl_leg', a=True, t=True)
-
-# creating no flip knee
-# create locator
-cmds.spaceLocator(n='lctrPv_leg')
-pelvisPos = cmds.xform('jnt_pelvis', q=True, ws=True, t=True)
-cmds.xform('lctrPv_leg', ws=True, t=pelvisPos)
-
-# set up polvector with new locator
-cmds.poleVectorConstraint ('lctrPv_leg', 'ikh_leg', weight=1)
-cmds.select("ctrl_leg")
-cmds.addAttr( shortName='Twist', longName='Twist', defaultValue=0, k=True)
-
-# Creating and connecting Nodes
-
-cmds.shadingNode("plusMinusAverage", asUtility=True, n='pmaNode_LegTwist')
-cmds.shadingNode("multiplyDivide", asUtility=True, n='mdNode_LegTwist')
-
-cmds.connectAttr('ctrl_leg.Twist', 'mdNode_LegTwist.input1X')
-cmds.connectAttr('ctrl_leg.ry', 'mdNode_LegTwist.input1Y')
-cmds.connectAttr('jnt_pelvis.ry', 'mdNode_LegTwist.input1Z')
-cmds.setAttr('mdNode_LegTwist.input2X', -1)
-cmds.setAttr('mdNode_LegTwist.input2Y', -1)
-cmds.setAttr('mdNode_LegTwist.input2Z', -1)
-cmds.connectAttr('mdNode_LegTwist.input1X', 'pmaNode_LegTwist.input1D[0]')
-cmds.connectAttr('mdNode_LegTwist.input1Y', 'pmaNode_LegTwist.input1D[1]')
-cmds.connectAttr('pmaNode_LegTwist.output1D', 'ikh_leg.twist')
+# cmds.select(kneeJnt, kneeLocTemp)
+# cmds.align(x='mid' ,alignToLead=True)
 
 
-
-# Creating the stretchy IK, start with creating the nodes, add attributes mesure distance and connect nodes
-
-cmds.shadingNode("addDoubleLinear", asUtility=True, n='adlNode_LegStretch')
-cmds.shadingNode("clamp", asUtility=True, n='clampNode_LegStretch')
-cmds.shadingNode("multiplyDivide", asUtility=True, n='mdNode_LegStretch')
-cmds.shadingNode("multiplyDivide", asUtility=True, n='mdNode_KneeStretch')
-cmds.shadingNode("multiplyDivide", asUtility=True, n='mdNode_AnkleStretch')
-
-# add stretch attribute to ctrl_leg
-
-cmds.select('ctrl_leg')
-cmds.addAttr( shortName='Stretch', longName='Stretch', defaultValue=0, k=True)
-
-# creating distance tool
-
-hipPos = cmds.xform('ikj_hip', q=True, ws=True, t=True)
-anklePos = cmds.xform('ikj_ankle', q=True, ws=True, t=True)
-disDim = cmds.distanceDimension(sp=(hipPos), ep=(anklePos))
-
-cmds.rename('distanceDimension1', 'disDimNode_legStretch')
-cmds.rename('locator1', 'lctrDis_hip')
-cmds.rename('locator2', 'lctrDis_ankle')
-cmds.parent('lctrDis_hip', 'jnt_pelvis')
-cmds.parent('lctrDis_ankle', 'grp_ball')
-
-kneeLen = cmds.getAttr('ikj_knee.tx')
-print kneeLen
-
-ankleLen = cmds.getAttr('ikj_ankle.tx')
-print ankleLen
-
-legLen = (kneeLen + ankleLen)
-print legLen
-
-cmds.setAttr('adlNode_LegStretch.input2', legLen)
-cmds.setAttr('mdNode_LegStretch.input2X', legLen)
-cmds.setAttr('mdNode_KneeStretch.input2X', kneeLen)
-cmds.setAttr('mdNode_AnkleStretch.input2X', ankleLen)
-
-# Connect the nodes to get the final stretch value that will be applied to our joints.
-# The clamp node lets us control the amount of stretch.
-
-cmds.connectAttr('ctrl_leg.Stretch', 'adlNode_LegStretch.input1')
-cmds.setAttr ("clampNode_LegStretch.minR", 12.800084)
-cmds.setAttr ("mdNode_LegStretch.operation",  2)
-
-cmds.connectAttr('disDimNode_legStretch.distance', 'clampNode_LegStretch.inputR')
-cmds.connectAttr( 'adlNode_LegStretch.output', 'clampNode_LegStretch.maxR')
-
-cmds.connectAttr('clampNode_LegStretch.outputR', 'mdNode_LegStretch.input1X')
-cmds.connectAttr('mdNode_LegStretch.outputX', 'mdNode_KneeStretch.input1X')
-cmds.connectAttr('mdNode_LegStretch.outputX', 'mdNode_AnkleStretch.input1X')
-
-cmds.connectAttr('mdNode_KneeStretch.outputX', 'ikj_knee.tx')
-cmds.connectAttr('mdNode_AnkleStretch.outputX', 'ikj_ankle.tx')
-
-# setting up foot, creating utility nodes and adding attributes
-
-cmds.select('ctrl_leg')
-cmds.addAttr( shortName='Roll_Break', longName='Roll_Break', defaultValue=0, k=True)
-cmds.addAttr( shortName='Foot_Roll', longName='Foot_Roll', defaultValue=0, k=True)
-
-cmds.shadingNode("condition", asUtility=True, n='conNode_ballRoll')
-cmds.shadingNode("condition", asUtility=True, n='conNode_negBallRoll')
-cmds.shadingNode("condition", asUtility=True, n='conNode_toeRoll')
-cmds.shadingNode("plusMinusAverage", asUtility=True, n='pmaNode_ballRoll')
-cmds.shadingNode("plusMinusAverage", asUtility=True, n='pmaNode_toeRoll')
-cmds.shadingNode("condition", asUtility=True, n='conNode_heelRoll')
-cmds.setAttr('pmaNode_toeRoll.operation', 2)
-cmds.setAttr ("conNode_toeRoll.operation", 2)
-cmds.setAttr ("conNode_toeRoll.colorIfFalseR", 0)
-cmds.setAttr ("conNode_toeRoll.colorIfFalseG", 0)
-cmds.setAttr ("conNode_toeRoll.colorIfFalseB", 0)
-cmds.setAttr ('conNode_heelRoll.operation', 4)
-cmds.setAttr('conNode_heelRoll.colorIfFalseB', 0)
-cmds.setAttr('conNode_heelRoll.colorIfFalseR', 0)
-cmds.setAttr('conNode_heelRoll.colorIfFalseG', 0)
-cmds.setAttr("pmaNode_ballRoll.operation", 2)
-cmds.setAttr ("conNode_negBallRoll.operation", 3)
-cmds.setAttr ("conNode_ballRoll.operation", 3)
-
-#setting up toe
-
-cmds.connectAttr('ctrl_leg.Foot_Roll', 'conNode_toeRoll.firstTerm')
-cmds.connectAttr('ctrl_leg.Foot_Roll', 'conNode_toeRoll.colorIfTrueR')
-cmds.connectAttr('ctrl_leg.Roll_Break', 'conNode_toeRoll.secondTerm')
-cmds.connectAttr('ctrl_leg.Roll_Break', 'conNode_toeRoll.colorIfFalseR')
-cmds.connectAttr('ctrl_leg.Roll_Break', 'pmaNode_toeRoll.input1D[1]')
-cmds.connectAttr('conNode_toeRoll.outColorR', 'pmaNode_toeRoll.input1D[0]')
-cmds.connectAttr('pmaNode_toeRoll.output1D', 'grp_toe.rx')
-
-# setting up heel
-
-cmds.connectAttr('ctrl_leg.Foot_Roll', 'conNode_heelRoll.firstTerm')
-cmds.connectAttr('ctrl_leg.Foot_Roll', 'conNode_heelRoll.colorIfTrueR')
-cmds.connectAttr('conNode_heelRoll.outColorR', 'grp_heel.rotateX')
-
-# setting up ball, and toe flap
-
-cmds.connectAttr('ctrl_leg.Foot_Roll', 'conNode_ballRoll.firstTerm')
-cmds.connectAttr('ctrl_leg.Foot_Roll', 'conNode_ballRoll.colorIfTrueR')
-cmds.connectAttr('ctrl_leg.Roll_Break', 'conNode_negBallRoll.secondTerm')
-cmds.connectAttr('ctrl_leg.Roll_Break', 'conNode_negBallRoll.colorIfTrueR')
-cmds.connectAttr('conNode_negBallRoll.outColorR', 'pmaNode_ballRoll.input1D[0]')
-cmds.connectAttr('grp_toe.rx', 'pmaNode_ballRoll.input1D[1]')
-cmds.connectAttr('pmaNode_ballRoll.output1D', 'grp_ball.rx')
-cmds.connectAttr('conNode_ballRoll.outColorR', 'conNode_negBallRoll.firstTerm')
-cmds.connectAttr('conNode_ballRoll.outColorR', 'conNode_negBallRoll.colorIfFalseR')
-
-cmds.select('ctrl_leg')
-cmds.addAttr( shortName='Toe_Flap', longName='Toe_Flap', defaultValue=0, k=True)
-cmds.connectAttr('ctrl_leg.Toe_Flap', 'grp_flap.rx')
-
-# set up Piviot for banck and twist
-
-cmds.circle(name='ctrl_footPivot', normal=(0,0,1), radius=(1))
-
-ballPos = cmds.xform('grp_ball', q=True, t=True, ws=True)
-cmds.xform('ctrl_footPivot', t=ballPos)
-
-cmds.group(n='grp_ctrl_footPivot', empty=True)
-
-cmds.parent('grp_ctrl_footPivot', 'ctrl_footPivot')
-
-cmds.parent('ctrl_footPivot', 'ctrl_leg')
-cmds.makeIdentity( apply=True )
-
-cmds.connectAttr('grp_ctrl_footPivot.translate', 'grp_footPivot.rotatePivot')
-
-cmds.xform('grp_ctrl_footPivot', t=ballPos)
-
-
-cmds.select('ctrl_leg')
-cmds.addAttr( shortName='Foot_Pivot', longName='Foot_Pivot', defaultValue=0, k=True)
-cmds.addAttr( shortName='Foot_Bank', longName='Foot_Bank', defaultValue=0, k=True)
-cmds.connectAttr('ctrl_leg.Foot_Pivot', 'grp_footPivot.ry')
-cmds.connectAttr('ctrl_leg.Foot_Bank', 'grp_footPivot.rz')
-
-'''
--- Stretch_plane.py
+++ Stretch_plane.py
@@ -3,7 +3,7 @@ import maya.cmds as cmds
 un=''
 numberOfBones=3
 
-#will need to add if statment to test for selected and joint
+# will need to add if statment to test for selected and joint
 startJoint = cmds.ls(selection=True, )
 endJoint = cmds.listRelatives(startJoint)[0]
 len = cmds.getAttr(endJoint+'.translateX')
@@ -11,37 +11,105 @@ len = cmds.getAttr(endJoint+'.translateX')
 startPoint = cmds.xform(startJoint, query=True, worldSpace=True, translation=True)
 endPoint =  cmds.xform(endJoint, query=True, worldSpace=True, translation=True)
 
-#create start and end locators and an empty group
+#create start + end locators, bones to skin the wire to, and an empty group
 
 startLoc = cmds.spaceLocator(name = 'startLoc')
 cmds.delete(cmds.parentConstraint(startJoint, startLoc))
-
 endLoc = cmds.spaceLocator(name = 'endLoc')
 cmds.delete(cmds.parentConstraint(endJoint, endLoc))
+
+cmds.select(clear=True)
+startWJoint = cmds.joint(name='startWireJoint')
+cmds.parent(startWJoint, startLoc, relative=True)
+cmds.select(clear=True)
+endWJoint = cmds.joint(name='endWireJoint')
+cmds.parent(endWJoint, endLoc, relative=True)
+
+
 
 sGrp = cmds.group(empty=True, name= un + 'stretchPlaneGrp')
 cmds.delete(cmds.parentConstraint(startLoc, endLoc, sGrp))
 cmds.delete(cmds.orientConstraint(startLoc, sGrp))
 
-#create NURBS plane and CV curve
+#create NURBS plane and CV curve. Group everything into the new sGrp
 
-sPlane = cmds.nurbsPlane(name= un + 'stretchPlane', width= len ,lengthRatio=.2, patchesU=numberOfBones, axis=(0,1,0) )
+sPlane = cmds.nurbsPlane(name= un + 'stretchPlane', width=len, lengthRatio=.2, patchesU=numberOfBones, axis=(0,1,0) )
+sPlaneShape = cmds.listRelatives(sPlane, shapes=True)[0]
 cmds.delete(cmds.parentConstraint(startLoc, endLoc, sPlane))
 cmds.delete(cmds.orientConstraint(startLoc, sPlane))
-sCurve = cmds.curve(name= un + 'stretchCurve', p=[startPoint,endPoint])
+cmds.delete(sPlane,constructionHistory=True)
 
-cmds.parent()
+sCurve = cmds.curve(degree=1, point=[startPoint, endPoint])
+sCurve = cmds.rename(sCurve, un + 'stretchCurve') # if name at the creation time, the shape doesn't get renamed
+
+cmds.parent(startLoc, endLoc, sPlane[0], sCurve, sGrp)
+
+#create wire deformer and dropoffLocator for twisting
+
+sWire = cmds.wire(sPlane[0], wire=sCurve, name=un + 'stretchWire')
+cmds.wire(sWire[1], edit=True, dropoffDistance=[(0,20),(1,20)])
+
+cmds.select( sWire[1] + '.u[0]', r=True )
+cmds.dropoffLocator( 1.0, 1.0, sWire[0] )
+cmds.select( sWire[1] + '.u[1]', r=True )
+cmds.dropoffLocator( 1.0, 1.0, sWire[0] )
+
+#skin wire to bone drivesr, connect rotation of locators to drop off locators twist
+
+
+cmds.connectAttr(startLoc[0]+'.rotateX',sWire[0]+'.wireLocatorTwist[0]')
+cmds.connectAttr(endLoc[0]+'.rotateX',sWire[0]+'.wireLocatorTwist[1]')
+
+cmds.skinCluster(startWJoint, endWJoint, sWire[1], maximumInfluences=1, toSelectedBones=True)
+
+#crating follicles on sPlane and joints
+
+sJointsCollection = []
+folGrp = cmds.group(empty=True, name= un + 'fillicleGrp')
+cmds.parent(folGrp, sGrp)
+
+for x in range (numberOfBones):
+    VPosition = (1.0/(numberOfBones+1))*(x+1)
+    folShape = cmds.createNode("follicle")
+    folTrans = cmds.listRelatives(folShape, parent=True)[0]
+    folTrans = cmds.rename(folTrans, un+"follicle_{:02d}".format(x + 1))
+    folShape = cmds.listRelatives(folTrans, shapes=True)[0]
+    folShape = cmds.rename(folShape, un+"follicleShape_{:02d}".format(x + 1))
+
+
+    cmds.connectAttr(sPlaneShape+'.worldMatrix[0]', folShape+'.inputWorldMatrix')
+    cmds.connectAttr(sPlaneShape+'.local', folShape+'.inputSurface')
+    cmds.connectAttr(folShape+'.outTranslate', folTrans+'.translate')
+    cmds.connectAttr(folShape + '.outRotate', folTrans + '.rotate')
+
+    cmds.setAttr(folShape+'.parameterU', (VPosition))
+    cmds.setAttr(folShape+'.parameterV', .5)
+
+    # here I'm adding a joint, as the follicle is selected the joint is parented under it.
+    # im also adding the joint to a list i can use later
+    sJointsCollection.append(cmds.joint(name=un+"stretchJoint_{:02d}".format(x + 1)))
+
+    cmds.parent(folTrans,folGrp)
+
+#
+# #creating nodes for stretching bones
+# sCurveShape = cmds.listRelatives(sCurve, shapes=True)[0]
+# sCurveInfo = cmds.createNode('curveInfo', name='SCurveInfo')
+# cmds.rename(sCurveInfo, 'SCurveInfo')
+# cmds.connectAttr(sCurveShape+'.worldSpace[0]', sCurveInfo+'.inputCurve')
+#
+# jointScale= cmds.createNode('multiplyDivide', name='sJointScale')
+# cmds.setAttr(jointScale+'.operation',2)
+# cmds.setAttr(jointScale+'input2X',len)
+# cmds.connectAttr(sCurveInfo+'.distance', jointScale+'.input1X')
+#
+# #connecting up nodes to bones
+# for joint in sJointsCollection:
+#     cmds.connectAttr(jointScale+'.outputX', joint+'.scaleX')
+
 '''
+visability of start wire joint
 
-
-create NURBS plane with length from second bone
-
-create cv curve
-
-create twist modifier 
-Make N hair
-deleat extra parts 
-parent bones to Nhair groups 
 
 group all parts except curve and Nhair/bones
 -----
